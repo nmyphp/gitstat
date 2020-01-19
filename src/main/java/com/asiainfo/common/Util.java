cba0acc444ce015819610940426417a97e827f13
nmyphp <544409242@qq.com>
2020-01-19 17:33:03 +0800

    init

---
+++ src/main/java/com/asiainfo/common/Util.java
@@ -0,0 +1,11 @@
+package com.asiainfo.common;
+
+import java.util.Date;
+
+public class Util {
+
+    public static int calcDifDays(Date end, Date start) {
+        long gap = end.getTime() - start.getTime();
+        return (int) Math.floor(gap / 86400000L);
+    }
+}
---
+++ src/main/java/com/asiainfo/constant/Constants.java
@@ -0,0 +1,5 @@
+package com.asiainfo.constant;
+
+public interface Constants {
+
+}
